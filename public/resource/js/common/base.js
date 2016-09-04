3da6d008b1e4ddb77ff8b33313746511140da8b3
lushijie <lushijie1218@126.com>
2016-09-04 15:53:12 +0800

    fix

--- public/resource/js/common/base.js
+++ public/resource/js/common/base.js
@@ -2,9 +2,8 @@
 * @Author: lushijie
 * @Date:   2016-03-03 14:31:32
 * @Last Modified by:   lushijie
-* @Last Modified time: 2016-09-03 11:05:15
+* @Last Modified time: 2016-09-04 15:53:05
 */
 
 import 'Rcss/common/reset.scss'
 console.log('This is base.js');
-console.log(1==2);
